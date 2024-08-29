34843a3cff987d84677da5f9fd5afd14a6a9d2d5
alok <aloktheaddiction@gmail.com>
2024-08-29 13:32:20 +0530

    get user details

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const app = express();
 app.get('/api/get' , (req,res) =>{
     res.send("hello user")
 })
+
+app.get('/api/get_user_details' , (req,res) => {
+    res.send({
+        user:{
+            name: "alok sharma",
+            age: 30,
+            contact : 1234567890
+        }
+    })
+})
 app.listen(8000, () => {
     console.log(`server is running on ${8000}`)
 })
